c1fd7d4b1a5bbc835b7fe31d0cf365baa14eaa38
leehong <virgil.leehong@gmail.com>
2012-10-05 00:45:04 +0800

    update

--- plugins/category_generator.rb
+++ plugins/category_generator.rb
@@ -165,9 +165,9 @@ module Jekyll
     #
     def category_links(categories)
       dir = @context.registers[:site].config['category_dir']
-       categories = categories.sort!.map do |item|
-    "<a class='category' href='/#{dir}/#{item[/(?<={)[^}]*/]}/'>#{item[/[^{]*/]}</a>"
-       end
+      categories = categories.sort!.map do |item|
+        "<a class='category' href='/#{dir}/#{item.gsub(/_|\P{Word}/, '-').gsub(/-{2,}/, '-').downcase}'>#{item}</a>"
+      end
 
       case categories.length
       when 0
